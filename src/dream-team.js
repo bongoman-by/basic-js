2db48a678ac583370e9aed5b83924be6acaaf347
bongoman-by <bongoman@tut.by>
2020-09-24 16:07:35 +0300

    feat: add subtask Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members === null || members === undefined || members === NaN) {
+    return false;  
+  }  if (members.constructor !== Array) {
+    return false;  
+  }
+  let array = members.filter(item => typeof(item) === 'string');
+  for(let index = 0; index < array.length; index++) {
+    array[index] = array[index].trim().toUpperCase();
+  }
+  array.sort();
+  let result = '';
+  for(let index = 0; index < array.length; index++) {
+    result = `${result}${array[index][0]}`;
+  }
+  return result;
 };
