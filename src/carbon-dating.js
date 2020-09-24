8a274b5909b7ea19a74b30544617aa1b0e540800
bongoman-by <bongoman@tut.by>
2020-09-24 11:32:34 +0300

    init: create BasicJS task and add subtasks Count cats! and Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const activity = +sampleActivity;
+  if (isNaN(NaN)) {
+    return false;  
+  }
+  if (typeof(activity) === 'number' && activity > 0) {
+    const result = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD));
+    if (result > 0) {
+      return result;
+    }
+    return false;
+  }
+  return false;
 };
