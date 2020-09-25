54889ad1e94eb6eb676c9aacadca8e20401cd117
bongoman-by <bongoman@tut.by>
2020-09-25 16:03:04 +0300

    feat: add subtasks Chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-const chainMaker = {
+let chainMaker = {
+  'chain': [],
+  'link': '~~',
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${(value === null) ? value : value.toString()} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    let index = +position - 1;
+    if (index > -1 && index < this.getLength()) {
+      this.chain.splice(index, 1);
+    } else {
+       try {
+        if (this.chain.indexOf(position) === -1) {
+          this.chain = [];
+          throw Error;     
+        }
+        this.chain.splice(this.chain.indexOf(position), 1);
+      } catch (error) {
+
+        this.chain = [];
+        throw Error;
+      }
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.join(this.link);
+    this.chain = [];
+    return result;
   }
 };
 
