c5b037017b0af19b3c025fc4440fc9f22e612692
bongoman-by <bongoman@tut.by>
2020-09-26 10:02:50 +0300

    feat: add subtask Recursive depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+
+   constructor(firstName, lastName) {
+     this.max = 0;    
+    }
+
+    calculateDepth(arr, depth = 0) {
+    if (depth === 0) {
+      this.max = 1;
+    }
+    depth = depth + 1;
+    arr.forEach(value => {
+       if (value === null || value === undefined || this.isNan(value)) {
+        } else if (value.constructor !== Array) {       
+         } else {
+         return this.calculateDepth(value, depth);
+       }
+      });
+      this.max = Math.max(depth, this.max);
+      console.log(`depth=${depth} max=${this.max}`);
+      depth = depth - 1;
+      return this.max;
+   }
+     
+    isNan(value) {
+      return value !== value;
+    }
+
+  };
